dc02c927b1c9c8d98c653f9989ac1280d42962b0
Nitin <nch2340@gmail.com>
2025-02-05 23:10:57 +0530

    sd

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const elements = document.querySelectorAll('[data-pf-type="ProductATC"]');
 
   // Define the redirect URL
-  const redirectUrl = "https://gandlebuz.com/";
+  const redirectUrl = "https://www.samsung.com/in/";
 
   elements.forEach((el) => {
       const rect = el.getBoundingClientRect(); // Get element's dimensions and position
